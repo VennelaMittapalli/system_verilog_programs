4db97acdacc6cf58f146c994fda3a25325b0d318
VennelaMittapalli <59090497+VennelaMittapalli@users.noreply.github.com>
2025-12-23 21:00:16 -0800

    Delete commented output section in struct.sv
    Removed commented output section from struct.sv

--- Datatypes/struct.sv
+++ Datatypes/struct.sv
@@ -8,7 +8,7 @@ module tb();
   end
 endmodule
 //OUTPUT
-/*
+/*collection/array of different types of datatypes, which is synthesizable
 Compiler version U-2023.03-SP2_Full64; Runtime version U-2023.03-SP2_Full64;  Dec 23 23:54 2025
 '{a:'h1, address:'h0, pari:error}
            V C S   S i m u l a t i o n   R e p o r t 
